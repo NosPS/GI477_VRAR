d2c3304c1002c87efdceb08a173df97910b45b5f
Phattaradanai <77877324+nos6996@users.noreply.github.com>
2022-07-02 15:36:15 +0700

    Update Bowling

---
+++ GI477_VR/Assets/BowlingGame/Scripts/ScoreManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e5d667650896eeab28fa87b2116330666fbe530bba173ba688a5767f5f4b6f93
+size 794
---
+++ GI477_VR/Assets/BowlingGame/Scripts/SpawnBall.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:718860514460f1cdf95e9c4f7fad4bd47d2093b51a589b93ff0bffb10658de5b
+size 700
